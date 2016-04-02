904b7f1958c046f8b89d875dd58621908fc62347
Samer Albahra <salbahra@gmail.com>
2016-04-02 18:17:14 -0500

    Auto populate make/model information

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,12 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('stockInVehicleCtrl', function($scope) {
+.controller('stockInVehicleCtrl', function($scope, CarAPI) {
+	$scope.data = {};
 
+	CarAPI.getAllModels( function( data ) {
+		$scope.data.makes = data.makes;
+	} );
 })
 
 .controller('vinCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,26 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory( "CarAPI", [ "$injector", "$rootScope", function( $injector, $rootScope ) {
+	var token = "63db4mypzb27b768uj4xp5qt",
+		$http;
 
+	return {
+		getAllModels: function( callback ) {
+			$http = $http || $injector.get( "$http" );
+
+            $http( {
+                method: "GET",
+                url: "https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=" + token
+            } ).then(
+                function( result ) {
+					callback( result.data );
+                },
+                function() {
+                    callback( false );
+                }
+            );
+		}
+	};
 }])
 
 .service('BlankService', [function(){
