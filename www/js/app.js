2f7bc2ad6f73729c5c582691a6117761ef420a0e
Samer Albahra <salbahra@gmail.com>
2016-04-02 18:58:18 -0500

    Fix missing dependency

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $timeout) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
