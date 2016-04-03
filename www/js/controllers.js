3bb021e352837f226500b505d8b48c4b415262b4
Samer Albahra <salbahra@gmail.com>
2016-04-03 11:41:32 -0500

    Auto populate make/model/year information on VIN scan

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,9 @@ angular.module( "app.controllers", [] )
 
 } )
 
-.controller( "stockInVehicleCtrl", function( $scope, CarAPI ) {
+.controller( "stockInVehicleCtrl", function( $scope, $filter, CarAPI ) {
+	var filterFilter = $filter( "filter" );
+
 	$scope.data = {};
 	$scope.info = {};
 
@@ -15,25 +17,39 @@ angular.module( "app.controllers", [] )
 	$scope.scanVIN = function() {
 		cordova.plugins.barcodeScanner.scan(
 			function( result ) {
-				console.log( result );
-				CarAPI.getVINInfo( result.text, function( data ) {
-					console.log( data );
-				} );
+				if ( result.text ) {
+					$scope.data.vin = result.text;
+					CarAPI.getVINInfo( result.text, function( data ) {
+						$scope.data.make = [ data.make ];
+						$scope.data.model = [ data.model ];
+						$scope.data.color = [ data.colors.options[ 0 ] ];
+					} );
+				}
 			}
 		);
 	};
 
+	window.testShit = function() {
+		result = {
+			text: "4T1BK1EB6DU056165"
+		};
+		$scope.data.vin = result.text;
+		CarAPI.getVINInfo( result.text, function( data ) {
+			$scope.data.make = $scope.info.makes[ $scope.info.makes.indexOf( filterFilter( $scope.info.makes, { id: data.make.id } )[ 0 ] ) ];
+			$scope.data.model = $scope.data.make.models[ $scope.data.make.models.indexOf( filterFilter( $scope.data.make.models, { id: data.model.id } )[ 0 ] ) ];
+			$scope.data.year = $scope.data.model.years[ $scope.data.model.years.indexOf( filterFilter( $scope.data.model.years, { id: data.years.id } )[ 0 ] ) ];
+			$scope.info.colors = data.colors[ 0 ].options;
+		} );
+	};
+
 	$scope.getColors = function() {
 		CarAPI.getColorForModel( $scope.data.make.name, $scope.data.model.name, $scope.data.year.year, function( data ) {
-			$scope.info.colors = data;
+			$scope.info.colors = data.styles[ 0 ].colors[ 1 ].options;
 		} );
 	};
 
 	$scope.saveCar = function() {
-		console.log( $scope.addCar );
-		if ( $scope.addCar.$valid ) {
-			console.log( $scope.data );
-		}
+		console.log( $scope.data );
 	};
 } )
 
